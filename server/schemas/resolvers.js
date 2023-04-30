1016e482357184f9921bd57fec7abd400b28ba39
anuffisenough <uffmanch@gmail.com>
2023-04-30 14:57:34 -0600

    adds provider logic

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,25 @@ const { User } = require('../models');
 const resolvers = {
     Query: {
         users: async () => {
-            return await User.find({});
-        }
-    }
+            return User.find();
+        },
+    },
+
+    Mutation: {
+        loginUser: async (parent, args) => {
+            return Auth;
+        },
+
+        addUser: async (parent, args) => {
+            return User.create(args);
+        },
+        saveBook: async (parent, args) => {
+            return User;
+        },
+        removeBook: async (parent, args) => {
+            return User;
+    },
+},
 };
 
 module.exports = resolvers;
