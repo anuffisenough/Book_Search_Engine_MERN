27a10e80ad9be82a0728ef5fc33fcb9622cf37c2
anuffisenough <uffmanch@gmail.com>
2023-05-17 14:47:32 -0600

    attempted Heroku connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://uffmanch:UniqueNewYork2023*@cluster0.zzbcked.mongodb.net/googlebooks?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
