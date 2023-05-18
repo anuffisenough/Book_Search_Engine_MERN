d7d98710f5e1a8769e02cef957d99f75b26e8a93
anuffisenough <uffmanch@gmail.com>
2023-05-17 18:27:28 -0600

    fixes the mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://uffmanch:UniqueNewYork2023*@cluster0.zzbcked.mongodb.net/googlebooks?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
